e96e32fe509fbb2e436a9ffe856be933d14110c5
Chan Chhaya <it.chhaya@gmail.com>
2026-08-15 16:29:19 +0700

    configure resource server

--- src/main/java/co/istad/chanchhaya/ecommerce/security/SecurityConfig.java
+++ src/main/java/co/istad/chanchhaya/ecommerce/security/SecurityConfig.java
@@ -41,7 +41,11 @@ public class SecurityConfig {
         );
 
         // 3. Authentication Mechanism (HTTP Basic Authentication, JWT, OAuth2)
-        http.httpBasic(Customizer.withDefaults());
+        //http.httpBasic(Customizer.withDefaults());
+        http.oauth2ResourceServer(
+                oauth2 -> oauth2
+                .jwt(Customizer.withDefaults())
+        );
 
         // 4. Disable CSRF
         http.csrf(AbstractHttpConfigurer::disable);
